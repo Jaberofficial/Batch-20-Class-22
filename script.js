99f805284b70ec653bc5436c6e953230590a0ac2
Jaberofficial <jaberhossainmojumderononto02@gmail.com>
2026-04-23 20:15:28 +0600

    Javascript Function Intro

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function sum(data1, data2) {
+
+    let number1 = data1;
+    let number2 = data2;
+
+    let sum = number1 + number2;
+
+    return sum;
+}
+
+let sumResult =
+    sum(40, 20);
+console.log(sumResult);
+
+console.log(sum(40, 80));
+
+
+function sub(data1, data2) {
+
+    let number1 = data1;
+    let number2 = data2;
+
+    let sub = number1 - number2;
+
+    return sub;
+}
+
+console.log(sub(90, 80));
